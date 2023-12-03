c9c2532f7ca1378ed4be544aae0722fae3a135f3
Dang van sinh <sinh.dang25@student.passerellesnumeriques.org>
2023-12-03 21:52:48 +0700

    push

--- 5. CHECKBOX-WITH-RANDOM/process.php
+++ 5. CHECKBOX-WITH-RANDOM/process.php
@@ -5,14 +5,15 @@ $colors = ['primary', 'danger', 'info', 'warning', 'success', 'secondary', 'dark
 $selectedIT = isset($_POST['IT']) ? $_POST['IT'] : [];
 ?>
 
+
 <div class="card">
-    <div class="card-body">
+    <div class="card-body d-flex flex-row flex-wrap">
         <?php
         $selectedCount = count($selectedIT);
         $colorIndex = 0;
         foreach ($selectedIT as $selectedSubject) {
             $color = $colors[$colorIndex % count($colors)];
-            echo '<div class="alert alert-' . $color . '">' . $selectedSubject . '</div>';
+            echo '<div class="alert alert-' . $color . ' mr-2 mb-2">' . $selectedSubject . '</div>';
             $colorIndex++;
         }
         ?>
